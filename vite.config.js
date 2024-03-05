80fdee85a59023dd64ff4e2ffa3e78bf70dd4973
batalova-kira <batalovakira@gmail.com>
2024-03-05 19:53:48 +0200

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: "/react-vite-template/", // Замініть на ім'я вашого репо
     plugins: [react()],
+    server: {
+        port: 3000,
+        open: true,
+    },
+    build: {
+        outDir: "./dist",
+        assetsDir: "assets",
+    },
+    css: {
+        modules: false,
+    },
+    base: "/react-vite-template",
 });
