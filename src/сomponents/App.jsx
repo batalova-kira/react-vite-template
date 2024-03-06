8f23612ae3b3e21b24c32867a244ad40b53ef31d
batalova-kira <batalovakira@gmail.com>
2024-03-06 23:31:49 +0200

    add styled-components globalStyle

---
+++ src/сomponents/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <p>Hello! It`s React Template </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
